495d5a26e14c08a1cce4dfab21aefa8abf2fbe12
JossPabloPC <51423507+JossPabloPC@users.noreply.github.com>
2022-10-31 19:10:57 -0600

    Update DungeonCreator.cs

--- HorrorGame/Assets/03_Scripts/Dungeon Creator/DungeonCreator.cs
+++ HorrorGame/Assets/03_Scripts/Dungeon Creator/DungeonCreator.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class DungeonCreator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int m_columns = 10;
+    int m_rows = 1;
+    PrimitiveList_Data m_sockets_data;
+
+    Wave_Function_Collapse wave_function_collapse;
+
+
+    Cell[,] m_grid;
+
+
     void Start()
     {
-        
+        wave_function_collapse = new Wave_Function_Collapse(m_columns, m_rows);
+        wave_function_collapse.Load_Primitives(m_sockets_data);
+        wave_function_collapse.Create_Grid();
+        wave_function_collapse.Create_Rules();
+        m_grid = wave_function_collapse.Run(true);
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
