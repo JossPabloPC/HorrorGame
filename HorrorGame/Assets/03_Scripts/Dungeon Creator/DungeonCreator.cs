898f1c1bdcb3c87b0fd5d85a1b9e562887639faf
JossPabloPC <51423507+JossPabloPC@users.noreply.github.com>
2022-11-01 15:28:13 -0600

    WFC implemented

--- HorrorGame/Assets/03_Scripts/Dungeon Creator/DungeonCreator.cs
+++ HorrorGame/Assets/03_Scripts/Dungeon Creator/DungeonCreator.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class DungeonCreator : MonoBehaviour
 {
-    int m_columns = 10;
-    int m_rows = 1;
-    PrimitiveList_Data m_sockets_data;
+    [SerializeField] private int                m_columns   = 10;
+    [SerializeField] private int                m_rows      = 1;
+    [SerializeField] private PrimitiveList_Data m_sockets_data;
 
     Wave_Function_Collapse wave_function_collapse;
 
@@ -20,6 +20,11 @@ public class DungeonCreator : MonoBehaviour
         wave_function_collapse.Load_Primitives(m_sockets_data);
         wave_function_collapse.Create_Grid();
         wave_function_collapse.Create_Rules();
+
+        wave_function_collapse.OverrideRowEntropy(0,new int[] {2,3,4});
+        wave_function_collapse.OverrideSingleCell(0, 0, new int[] {0});
+        wave_function_collapse.OverrideSingleCell(0, m_columns - 1, new int[] {1});
+
         m_grid = wave_function_collapse.Run(true);
 
     }
