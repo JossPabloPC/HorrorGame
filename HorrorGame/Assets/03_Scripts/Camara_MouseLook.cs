da464a3c0cba8bd403f807774fa2baf8c28f3584
Santiri2mil <santiago2mil@gmail.com>
2022-11-02 19:30:39 -0600

    Move and Shoot
    La camara ya se mueve correctamente, y e jugador se mueve acorde a la camara; el disparo ya funciona y detecta correctamente el objeto impactado, ademas se puede elegir entre modo ametralladora o modo pistola.

---
+++ HorrorGame/Assets/03_Scripts/Camara_MouseLook.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara_MouseLook : MonoBehaviour
+{
+    [SerializeField] private float mouseSenitivity=100f;
+    [SerializeField] private float mouseX,mouseY;
+    private float xRotation=0f;
+    public Transform playerBody;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState=CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX=Input.GetAxis("Mouse X")*mouseSenitivity*Time.deltaTime;//Coordenadas del mouse en x
+        mouseY=Input.GetAxis("Mouse Y")*mouseSenitivity*Time.deltaTime;//cordenadas del mouse en y
+        xRotation-=mouseY;//Rotacion en Y
+        xRotation=Mathf.Clamp(xRotation,-90f,90f);//limitamos la rotación en Y
+        transform.localRotation=Quaternion.Euler(xRotation,0f,0f);//aplicamos rotación en Y
+        playerBody.Rotate(Vector3.up*mouseX);//aplicamos rotacion en X
+
+    }
+}
