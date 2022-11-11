f9035c8ccad820ef35792768094df5dc50479b32
GranOla18 <80288848+GranOla18@users.noreply.github.com>
2022-11-11 13:20:19 -0600

    States Machine feat & NavMesh feat
    Used @JossPabloPC States Machine scripts for the enemy behaviour. It has an Idle state and a ChasePlayer state. It enters Idle bu default and when the player enters its trigger of field of view it will change to the chase player state, when the player exits this collider it will return to idle.
    
    Also implemented the navmesh so that it moves towards the "player" in a given area.

---
+++ HorrorGame/Assets/03_Scripts/Enemy/EnemyIdle.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyIdle : State
+{
+    bool canIdle;
+    public override void Enter()
+    {
+        base.Enter();
+        Debug.Log("Estoy en Idle");
+        canIdle = true;
+        //m_stateMachine.ChangeState(m_character.chase);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        canIdle = false;
+    }
+
+    private void Update()
+    {
+        if (canIdle)
+        {
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer != 0 && other.gameObject.layer != 7)
+        {
+            m_stateMachine.ChangeState(m_character.chase);
+            Debug.Log("webos");
+        }
+    }
+}
