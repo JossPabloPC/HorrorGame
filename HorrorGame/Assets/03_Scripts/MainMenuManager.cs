bc18854399a680f11e69a494d46ca57b2073b923
mar-amieva16 <0222474@up.edu.mx>
2022-11-15 13:09:33 -0600

    Created main menu scene
    Created a new scene for the main menu and added the main elements of the canvas, as well as the necessary buttons.

---
+++ HorrorGame/Assets/03_Scripts/MainMenuManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void Jugar()
+    {
+        //SceneManager.LoadScene(1);
+    }
+
+    public void Opciones()
+    {
+
+    }
+
+    public void Creditos()
+    {
+
+    }
+
+    public void Volver()
+    {
+
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
