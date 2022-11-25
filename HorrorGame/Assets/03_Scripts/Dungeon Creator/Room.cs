dab58897ccf1b303f76609d60e0290345500cee5
JossPabloPC <51423507+JossPabloPC@users.noreply.github.com>
2022-11-24 19:29:06 -0600

    High Level navmesh added

--- HorrorGame/Assets/03_Scripts/Dungeon Creator/Room.cs
+++ HorrorGame/Assets/03_Scripts/Dungeon Creator/Room.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Room : MonoBehaviour
 {
     public Transform m_entrance;
     public Transform m_exit;
+
+    [SerializeField] private NavMeshSurface [] romsSurfaces;
+
+    private void OnEnable()
+    {
+        Debug.Log("CreateNavMesh");
+    }
 }
