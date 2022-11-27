f33e7e9249919f04b73cf78983ff2a8df2763361
Santiri2mil <santiago2mil@gmail.com>
2022-11-27 12:47:47 -0600

    Mejoras Movimiento
    Por desgracia aun hay errores, trabajare para corregirlos

--- HorrorGame/Assets/03_Scripts/Gun.cs
+++ HorrorGame/Assets/03_Scripts/Gun.cs
@@ -1,6 +1,7 @@
 //using System.Collections;
 //using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gun : MonoBehaviour
 {
@@ -8,27 +9,97 @@ public class Gun : MonoBehaviour
     public float range=100f;
     public float impactForce=10f;
     public ParticleSystem muzzFlash; 
+    private bool canShoot=false;
+
+    [SerializeField] private Transform pistolPosition;
     //public GameObject impactEffect;
+<<<<<<< Updated upstream
     public Camera fpsCam;
+=======
+
+    //Reloading
+    public int maxAmmo=6;
+    private int currentAmmo;
+    public float reloadTime;
+    [SerializeField]private bool isReloading=false;
+    public Animator animatorWeapon;
+    public Image reloadProgres;
+    
+
+>>>>>>> Stashed changes
     //Disparo rapido
     public float fireRate=15f;
     private float nextTimeFire =0f;
     [SerializeField] private bool pistol;
 
+<<<<<<< Updated upstream
     // Update is called once per frame
     void Update()
     {
         if(Input.GetButton("Fire1")&&Time.time>=nextTimeFire&&!pistol)//Disparos continuos(ametralladora)
+=======
+    private void Start() {
+        currentAmmo=maxAmmo;
+        pistolPosition.transform.position=this.transform.localPosition;
+        reloadProgres.fillAmount=0;
+    }
+    /*
+    onEnable()//En caso de que queramos aumentar el número de armas para evitar bugs
+    {
+        isReloading=false;
+        animator.SetBool("Reload",false);
+    }
+    */
+    // Update is called once per frame
+    void Update()
+    {
+        if(isReloading){//si estamos recargando no queremos hacer todo lo demas
+            if(Input.GetButtonUp("Fire2"))//Dejamos de apuntar
+            {
+                PlayerMovement.pmInstance.canMove=true;
+                canShoot=false;
+            }
+            return;
+        }
+        if(currentAmmo<=0)//si nos acabamos la municion recargamos
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+       InputShoot();
+    }
+
+    private void InputShoot(){
+
+         //Inputs
+         if(Input.GetButton("Fire2"))//Apuntamos
+         {
+            //this.transform.SetParent(fpsCam.transform);
+            PlayerMovement.pmInstance.canMove=false;
+            canShoot=true;
+            
+         }
+         if(Input.GetButtonUp("Fire2"))//Dejamos de apuntar
+        {
+            //this.transform.localPosition= pistolPosition.transform.position;
+            //this.transform.SetParent(PlayerMovement.pmInstance.transform);
+            PlayerMovement.pmInstance.canMove=true;
+            canShoot=false;
+            
+        }
+        if(Input.GetButton("Fire1")&&Time.time>=nextTimeFire&&!pistol&&canShoot)//Disparos continuos(ametralladora) si podemos apuntar
+>>>>>>> Stashed changes
         {
             nextTimeFire=Time.time+1f/fireRate;//cadencia de disparo por segundo
             Shoot();
         }
-        if(Input.GetButtonDown("Fire1")&&pistol)//Disparos controlados (pistola)
+        if(Input.GetButtonDown("Fire1")&&pistol&&canShoot)//Disparos controlados (pistola) si podemos apuntar
         {
             
             Shoot();
         }
     }
+    
 
     void Shoot()
     {
@@ -37,6 +108,7 @@ public class Gun : MonoBehaviour
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit,range))
         {
             Debug.Log(hit.transform.name);
+<<<<<<< Updated upstream
             if(hit.rigidbody)
             {
                 hit.rigidbody.AddForce(hit.normal*impactForce);//LE añadimos una fuerza al objeto impactado
@@ -53,5 +125,32 @@ public class Gun : MonoBehaviour
             */
         }
     }
+=======
+        }
+    }
+    IEnumerator Reload ()
+    {
+        float timeTrans=0f;
+        isReloading=true;//Se esta recargando
+        Debug.Log("Reloading...");
+        animatorWeapon.SetBool("Reload",true);
+        while(timeTrans<reloadTime)
+        {
+            reloadProgres.fillAmount=Mathf.Lerp(0,1,timeTrans);
+            reloadProgres.fillAmount=0;
+            
+
+            timeTrans+=Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        //yield return new WaitForSeconds(reloadTime);//esperamos n segundos a que recargue
+        currentAmmo=maxAmmo;//volvemos a rellanar la municion al maximo
+        animatorWeapon.SetBool("Reload",false);
+        isReloading=false;//No se esta recargando
+        
+
+        
+    }
+>>>>>>> Stashed changes
 
 }
--- HorrorGame/Assets/03_Scripts/PlayerMovement.cs
+++ HorrorGame/Assets/03_Scripts/PlayerMovement.cs
@@ -1,29 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //Movimiento jugador
+//Movimiento jugador
+    public static PlayerMovement pmInstance;//Singleton 
     [SerializeField]private float xInput, zInput;
     public CharacterController controller;
-    public float speed=12f;
-    public float jumpHeight=3;
+    private Vector3 move;
+    public float speed=7f;
+    public float sprintSpeed=11f;
+    [SerializeField] private bool canSprint=true;
+    public float jumpHeight=1.5f;
+    public Slider sprintStamina;
+    private float staminaDecrese=0.5f, staminaIncrease=0.8f;
+    [SerializeField]
+    private float sprintRecharge;
+
 
     //Gravedad
-    [SerializeField] private Vector3 velocity;
-    public float gravity=-9.81f;
+    private Vector3 velocity;
+    public float gravity=-18.81f;
     
     //Ground CHECK
     public Transform groundCheck;
     public float groundDistance=0.4f;
     public LayerMask groundMask;
     [SerializeField] private bool isGrounded;
+    public bool canMove=true;
     // Start is called before the first frame update
+<<<<<<< Updated upstream
     void Start()
     {
         
     }
+=======
+    
+    private void Awake() 
+    {
+        if (pmInstance != null && pmInstance != this) 
+        { 
+            Destroy(this); 
+        } 
+        else 
+        { 
+            pmInstance = this; 
+        } 
+    }
+
+>>>>>>> Stashed changes
 
     // Update is called once per frame
     void Update()
@@ -33,16 +60,59 @@ public class PlayerMovement : MonoBehaviour
         if(isGrounded&&velocity.y<0){//Ajustamos la velocidad en Y al tocar el suelo
             velocity.y=-2f;
         }
-        if(Input.GetButtonDown("Jump")&&isGrounded)//brinco
+
+        if(Input.GetButtonDown("Jump")&&isGrounded&&canMove)//brinco
         {
             velocity.y=Mathf.Sqrt(jumpHeight*-2*gravity);//ecuacion que nos indica la velocidad necesaria para llegar una altura requerida
         }
+
         xInput=Input.GetAxis("Horizontal");
         zInput=Input.GetAxis("Vertical");
-        Vector3 move = transform.right*xInput+transform.forward*zInput;//Calculamos la direccion de nuestro movimiento
-        controller.Move(move*speed*Time.deltaTime);//Aplicamos movimiento
+        move = transform.right*xInput+transform.forward*zInput;//Calculamos la direccion de nuestro movimiento
+        if(canMove)//Movimiento
+        {
+            controller.Move(move*speed*Time.deltaTime);//Aplicamos movimiento
+        }
+        
         velocity.y+=gravity*Time.deltaTime;//Calculamos la gravedad
         controller.Move(velocity*Time.deltaTime);//Aplicamos gravedad
-    
+
+        SprintInputs();
+        
     }
+<<<<<<< Updated upstream
+=======
+    private void SprintInputs(){
+        if(Input.GetKey(KeyCode.LeftShift)&&canSprint)//Sprint
+        {
+            sprintStamina.value-=staminaDecrese*Time.deltaTime;
+            controller.Move(move*sprintSpeed*Time.deltaTime);
+            
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            StartCoroutine(StaminaRecharge(sprintStamina.value));   
+        }
+        if(sprintStamina.value<0.1f)
+        {
+            StartCoroutine(StaminaRecharge(sprintStamina.value));
+        }
+    }
+    public IEnumerator StaminaRecharge(float staminaV)
+    {
+        float timeTrans=0f;
+        canSprint=false;
+        while(timeTrans<sprintRecharge)
+        {
+            
+            sprintStamina.value+=staminaIncrease*Time.deltaTime;
+            timeTrans+=Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        canSprint=true;
+        
+        
+    }
+
+>>>>>>> Stashed changes
 }
