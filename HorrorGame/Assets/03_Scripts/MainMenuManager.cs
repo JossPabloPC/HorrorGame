0d2ab47302966d5fffe7ab78b69360701cd50f02
mar-amieva16 <0222474@up.edu.mx>
2022-11-27 21:09:22 -0600

    Created HUD
    Created a new scene for the game's HUD and added everything (still needs functionality depending on other scripts)

--- HorrorGame/Assets/03_Scripts/MainMenuManager.cs
+++ HorrorGame/Assets/03_Scripts/MainMenuManager.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public Text btn_txt;
     public void Jugar()
     {
         //SceneManager.LoadScene(1);
