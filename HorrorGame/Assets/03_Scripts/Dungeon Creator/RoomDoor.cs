35aebab479a29d210ff19302077bd62f56130a47
JossPabloPC <51423507+JossPabloPC@users.noreply.github.com>
2022-11-28 12:42:18 -0600

    intro room changed

--- HorrorGame/Assets/03_Scripts/Dungeon Creator/RoomDoor.cs
+++ HorrorGame/Assets/03_Scripts/Dungeon Creator/RoomDoor.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(BoxCollider))]
 public class RoomDoor : MonoBehaviour
 {
+    public UnityEvent   onDoorOpen;
     private BoxCollider m_collider;
     private void Start()
     {
@@ -18,6 +20,7 @@ public class RoomDoor : MonoBehaviour
         {
             DungeonCreator.Instace.DisplayNextRoom();
             m_collider.enabled = false;
+            onDoorOpen.Invoke();
         }
     }
 }
